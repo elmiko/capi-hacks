0b913c7eb99b5cf5dfed51e7aa8c4776d122fbbf
michael mccune <msm@opbstudios.com>
2022-06-04 22:10:39 -0400

    update versions
    * kubernetes
    * cluster-api
    * capk

--- 03-clusterctl-init.sh
+++ 03-clusterctl-init.sh
@@ -6,12 +6,13 @@ else
     kubeconfig_flag="--kubeconfig $1"
 fi
 
-capi_version="v1.0.99"
+capi_version="v1.2.99"
+capk_version="v0.3.99"
 
 clusterctl init \
     --core cluster-api:$capi_version \
     --bootstrap kubeadm:$capi_version \
     --control-plane kubeadm:$capi_version \
-    --infrastructure kubemark:$capi_version,docker:$capi_version \
+    --infrastructure kubemark:$capk_version,docker:$capi_version \
     --config ~/.cluster-api/dev-repository/config.yaml $kubeconfig_flag
 
